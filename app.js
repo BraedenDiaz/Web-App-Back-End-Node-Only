b5ab7ff28916661768deb353b708aaf6f16e47dd
Braeden Diaz <braedendiaz@gmail.com>
2021-12-17 23:38:53 -0700

    Initial setup and configuration. Create directory structure. Create a working server and homepage.

---
+++ app.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const http = require("http");
+const fs = require("fs");
+const util = require("util");
+
+const { SERVER_PORT } = require("./config/config");
+
+function readFileV2(file)
+{
+    return util.promisify(fs.readFile)(file);
+}
+
+async function handleGetRequests(req, res)
+{
+    const requestURL = new URL(req.url, `http://${req.headers.host}`);
+    const pathname = requestURL.pathname;
+
+    if (pathname === "/")
+    {
+        const indexHTML = await readFileV2("./views/index.html");
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(indexHTML);
+        res.end();
+    }
+    else
+    {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.write("404 File Not Found.");
+        res.end();
+    }
+}
+
+const server = http.createServer((req, res) => {
+
+    switch (req.method)
+    {
+        case "GET":
+            handleGetRequests(req, res);
+            break;
+        case "POST":
+            res.writeHead(405, {"Content-Type": "text/html"});
+            res.write("405 Method Not Allowed.");
+            res.end();
+            break;
+        case "PUT":
+            res.writeHead(405, {"Content-Type": "text/html"});
+            res.write("405 Method Not Allowed.");
+            res.end();
+            break;
+        case "DELETE":
+            res.writeHead(405, {"Content-Type": "text/html"});
+            res.write("405 Method Not Allowed.");
+            res.end();
+            break;
+        default:
+            res.writeHead(400, {"Content-Type": "text/html"});
+            res.write("400 Bad Request.");
+            res.end();
+    }
+});
+
+server.listen(SERVER_PORT, () => {
+    const { address, port } = server.address();
+    console.log(`Web server is listening at ${address}:${port}...`);
+});
---
+++ config/config.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const SERVER_PORT = 8080;
+
+module.exports.SERVER_PORT = SERVER_PORT;
